4bd39314d9e54661dd79b8b046021d69d5c9d011
Rahul <Rahul@Rahul-PC>
2014-03-17 12:04:59 -0400

    adding in memeory map for peer info

---
+++ Code/src/com/uf/cise/sp14/cnt/project/remotePeerMgr/PeersMap.java
@@ -0,0 +1,31 @@
+package peersManager;
+
+import java.util.HashMap;
+
+/**
+ * @author Rahul
+ *
+ */
+public class PeersMap {
+	
+	/**
+	 * this is a mapping of peer ID and the peer info of all the peers in the connection
+	 */
+	HashMap<Integer,RemotePeerInfo> peersMap = new HashMap<Integer,RemotePeerInfo>();
+
+	public HashMap<Integer, RemotePeerInfo> getPeersMap() {
+		return peersMap;
+	}
+
+	public void setPeersMap(HashMap<Integer, RemotePeerInfo> peersMap) {
+		this.peersMap = peersMap;
+	}
+
+	@Override
+	public String toString() {
+		return "PeersMap [peersMap=" + peersMap + "]";
+	} 
+	
+	
+
+}
